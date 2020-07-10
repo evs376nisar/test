0858ee291e7a25aae7ea26a62a22a5b2462cd975
Perfect Dakku <mohammadnisar316@gmail.com>
2020-07-10 23:09:18 +0500

    i added c++ file to master branch

---
+++ c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>	#include<iostream>
+using namespace std;	
+int main (){	
+
+    int num=2;
+    int num2=3;
+    bool result=num > num2;
+    cout<<result;
+    
+   
+    return 0;
+
+}
